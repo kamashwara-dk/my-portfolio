2a82ae00f211368ba55823476afeea9003e52ed0
KAMASHWARA D K <kamashwaradk@gmail.com>
2026-02-05 09:32:21 +0530

    Fix casing and exports

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,34 +1,24 @@
-// Contains components to be exported in other files
-// ! Don't remove anything from here
-
-import { About } from "./About";
-import {
-  BallCanvas,
-  ComputersCanvas,
-  EarthCanvas,
-  StarsCanvas,
-} from "./canvas";
-import { Contact } from "./Contact";
-import { Experience } from "./Experience";
-import { Feedbacks } from "./Feedbacks";
-import { Hero } from "./Hero";
-import { Navbar } from "./Navbar";
-import { Tech } from "./Tech";
-import { Works } from "./Worksorks";
-import { Certifications } from "./Certifications";  
+import { EarthCanvas, BallCanvas, ComputersCanvas, StarsCanvas } from './canvas';
+import { Hero } from './Hero';
+import { Navbar } from './Navbar';
+import { About } from './About';
+import { Tech } from './Tech';
+import { Experience } from './Experience';
+import { Works } from './Works';
+import { Contact } from './Contact';
+import { Certifications } from './Certifications';
 
 export {
-  About,
-  BallCanvas,
-  ComputersCanvas,
-  Contact,
-  EarthCanvas,
-  Experience,
-  Feedbacks,
   Hero,
   Navbar,
-  StarsCanvas,
+  About,
   Tech,
+  Experience,
   Works,
-  Certifications,
+  Contact,
+  EarthCanvas, 
+  BallCanvas, 
+  ComputersCanvas, 
+  StarsCanvas,
+  Certifications
 };
